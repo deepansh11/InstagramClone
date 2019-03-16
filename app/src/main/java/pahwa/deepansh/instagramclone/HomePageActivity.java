2719957b2372c33e6647ed6b6ea21a7705856a2a
deepansh11 <34342846+deepansh11@users.noreply.github.com>
2019-03-16 18:42:40 +0530

    Share Image Completed

--- app/src/main/java/pahwa/deepansh/instagramclone/HomePageActivity.java
+++ app/src/main/java/pahwa/deepansh/instagramclone/HomePageActivity.java
@@ -1,11 +1,33 @@
 package pahwa.deepansh.instagramclone;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.io.ByteArrayOutputStream;
 
 public class HomePageActivity extends AppCompatActivity {
 
@@ -19,6 +41,7 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        setTitle("HOME PAGE!!");
 
         toolbar = findViewById(R.id.myToolbar);
         setSupportActionBar(toolbar);
@@ -29,7 +52,102 @@ public class HomePageActivity extends AppCompatActivity {
         viewPager.setAdapter(tabAdapter);
         tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager,true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.my_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.postImageItem){
+
+            if (Build.VERSION.SDK_INT >= 23 && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},3000
+                        );
+            } else {
+                captureImage();
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 3000){
+
+            if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+
+                captureImage();
+
+        }
 
 
+    }
+
+    private void captureImage() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 4000);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 4000 && resultCode == RESULT_OK && data != null){
+
+            try{
+
+                Uri captredImage = data.getData();
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),captredImage);
+
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+                bitmap.compress(Bitmap.CompressFormat.PNG,100,byteArrayOutputStream);
+
+                byte[] bytes = byteArrayOutputStream.toByteArray();
+
+                ParseFile parseFile = new ParseFile("pic.png",bytes);
+                ParseObject parseObject = new ParseObject("Photo");
+                parseObject.put("picture",parseFile);
+                parseObject.put("username", ParseUser.getCurrentUser().getUsername());
+
+                final ProgressDialog dialog = new ProgressDialog(this);
+
+                dialog.setMessage("Loading..");
+                dialog.show();
+
+                parseObject.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if (e == null) {
+                            FancyToast.makeText(HomePageActivity.this, "Task Complete!!", Toast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
+                        } else {
+                            FancyToast.makeText(HomePageActivity.this, "Unknown Error: " + e.getMessage(), Toast.LENGTH_SHORT, FancyToast.ERROR, true).show();
+                        }
+                        dialog.dismiss();
+                    }
+                });
+                }catch (Exception e){
+                e.printStackTrace();
+            }
+        }else {
+            FancyToast.makeText(HomePageActivity.this, "Error: You Must Select an Image!!", FancyToast.LENGTH_SHORT, FancyToast.WARNING, true).show();
+        }
     }
 }
